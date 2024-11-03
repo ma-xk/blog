d5abff2b09b2ab222cc38bdc903ec210161cbcd0
max <95646268+ma-xk@users.noreply.github.com>
2024-11-03 16:36:03 +0000

    eslint fix & sitemap updates

---
+++ app/sitemap.js
@@ -0,0 +1,30 @@
+import { getAllPosts } from '../lib/posts';
+
+export async function GET() {
+  const baseUrl = 'https://maxlk.com'; // Use your actual domain
+
+  const posts = getAllPosts();
+
+  // Generate the XML for each blog post
+  const sitemapEntries = posts.map((post) => {
+    return `
+      <url>
+        <loc>${baseUrl}/blog/${post.slug}</loc>
+        <lastmod>${post.date}</lastmod>
+      </url>
+    `;
+  }).join('');
+
+  // Complete the XML structure for the sitemap
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${sitemapEntries}
+    </urlset>
+  `;
+
+  return new Response(sitemap, {
+    headers: {
+      'Content-Type': 'application/xml',
+    },
+  });
+}
